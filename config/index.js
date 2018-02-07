c3c4b8402a8e1ed794baf8a10ab35070567aa5b7
xushaowei <xushaowei10077@autohome.com.cn>
2018-02-07 17:35:53 +0800

    初始化项目

--- config/index.js
+++ config/index.js
@@ -1,6 +1,6 @@
 module.exports = {
-  port: 3008,
-  dbPth : ['mongodb://127.0.0.1:27017/','test']
+  port: 3010,
+  dbPth : ['mongodb://127.0.0.1:27017/','hotel']
   // dbPth : ['mongodb://xushaowei:xu2121836@ds121696.mlab.com:21696/','xutest']
 };
 
